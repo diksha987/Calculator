aa95a98c45d3bbf01e6d6c6913459339b1cc2c42
Diksha Gupta <56405230+diksha987@users.noreply.github.com>
2022-09-18 23:51:28 +0530

    Add files via upload

---
+++ c.js
@@ -0,0 +1,16 @@
+function view(num)
+{
+    document.getElementById("result"),value+=num
+}
+
+function compute()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+function clr()
+{
+    document.getElementById("result").value = ""
+}
